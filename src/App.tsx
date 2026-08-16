bfe48a6cd9acb6e2949962dfa9207c9a11b13a35
Dilshit <dilshikk@mail.ru>
2026-08-16 07:15:05 +0500

    Add /catering route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { DefaultProviders } from "./components/providers/default.tsx";
 import Index from "./pages/Index.tsx";
 import Story from "./pages/Story.tsx";
+import Catering from "./pages/catering/page.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/story" element={<Story />} />
+          <Route path="/catering" element={<Catering />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
